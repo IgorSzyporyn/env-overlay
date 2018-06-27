9eee04de5ce9529c0d8a71b685a7c9275858a148
IgorJoergensen <ij@omnicar.dk>
2018-06-27 13:22:52 +0200

    kicking off tests for the overlay itself

---
+++ src/envOverlay.test.ts
@@ -0,0 +1,7 @@
+import { envOverlay } from './envOverlay'
+import { wait } from 'dom-testing-library'
+import 'dom-testing-library/extend-expect'
+
+describe('envOverlay()', () => {
+  envOverlay('demo')
+})
