cd5d2028ed2ff78672b98cfbba8bdd276745421b
IgorSzyporyn <igor.szyporyn@gmail.com>
2020-08-16 14:51:04 +0200

    fix(storybook): console output the callback when loaded

--- react-app-example/src/index.tsx
+++ react-app-example/src/index.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { envOverlay } from './dist'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { envOverlay } from 'env-overlay'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
 
 envOverlay(process.env.NODE_ENV, {
   corner: 'left',
   onLoaded: () => {
     console.log('Im loaded now')
-  }
+  },
 })
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById('root'),
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
